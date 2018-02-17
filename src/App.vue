68b676c4c7c5245d4344f6494ec2b9be82bea05b
infernalslam <infernal-slam@hotmail.com>
2018-02-17 22:31:06 +0700

    add side bar :building_construction:

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,20 @@
 <template>
-  <router-view></router-view>
+<div class="columns">
+  <div class="column is-2">
+    <side-menu />
+  </div>
+  <div class="column">
+    <router-view />
+  </div>
+</div>
 </template>
 
 <script>
+import sideMenu from '@/pages/menu'
 export default {
-  name: 'app'
+  name: 'app',
+  components: {
+    sideMenu
+  }
 }
 </script>
---
+++ src/pages/menu.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <aside class="menu">
+      <p class="menu-label">Emogit</p>
+      <ul class="menu-list">
+        <li><a>EMOJI_CHEAT_SHEET</a></li>
+        <li>
+          <a class="is-active">succees code</a>
+          <ul>
+            <li><a>success</a></li>
+            <li><a>bug</a></li>
+            <li><a>pull request</a></li>
+          </ul>
+        </li>
+        <li><a>repository</a></li>
+        <li><a>Fierbase And github: ...........</a></li>
+        <li><a>Made withby ❤️ Takz</a></li>
+      </ul>
+    </aside>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style scoped>
+
+</style>
